ad9e0661a2e125d45ab819a98dee9779b7f5eb2e
wyh13935351452 <1430249861@qq.com>
2022-05-28 13:23:07 +0800

    做完了hearder静态

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-   跟组件
+   <Header></Header>
+   <router-view></router-view>
+   <!-- 登录注册不显示footer -->
+   <Footer v-show="$route.meta.show"></Footer>
   </div>
 </template>
 
 <script>
-
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 export default {
-  
+  components:{Header,Footer}
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
-
+//引入路由组件
+import router from '@/router'
+//注册全局组件
+import TypNav from '@/pages/Home/TypNav'
+Vue.component(TypNav.name,TypNav)
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
